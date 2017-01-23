358f2c43235f7125cf906a7b236d736d65b5b3d0
ice <smalltides@gmail.com>
2017-01-23 17:43:43 +0800

    use variable and function

---
+++ Script.py
@@ -0,0 +1,10 @@
+
+print('First Program')
+replaced = 'Ardit'.replace('A', 'O')
+print(replaced)
+
+currency = 145
+currency_today = 100.5
+currency1 = 'nothing'
+
+print(currency, currency_today, currency1)
---
+++ Script2.py
@@ -0,0 +1,7 @@
+print(1)
+
+def currency_converter(rate, euros):
+    dollars = euros * rate
+    return dollars
+
+print(currency_converter(100, 1000))
