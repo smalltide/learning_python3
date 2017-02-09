21b4e471de36f68e4ae64e228da4038be36f5b31
ice <smalltides@gmail.com>
2017-02-09 16:43:16 +0800

    use file read and write

--- C2F.py
+++ C2F.py
@@ -1,11 +1,26 @@
 def c2f(c):
     if -273.15 > c:
-        return "temperature too lower"
+        return ""
     else:
         f = c * 9 / 5 + 32
         return f
 
-temperatures=[10, -20, -289, 100]
+with open('example.txt', 'r') as file:
+    temperatures = file.readlines()
+
+temperatures = [float(t.rstrip()) for t in temperatures]
 
 for t in temperatures:
     print(c2f(t))
+
+
+temperatures=[10,-20,-289,100]
+
+def writer(temperatures):
+    with open("example2.txt", 'w') as file:
+        for c in temperatures:
+            if c>-273.15:
+                f=c*9/5+32
+                file.write(str(f)+"\n")
+
+writer(temperatures)
