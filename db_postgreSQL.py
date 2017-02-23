7972e274bf74d4d8e5c34e4f947890a88752f7fd
ice <smalltides@gmail.com>
2017-02-23 22:03:53 +0800

    use psycopg2 to operate  postgreSQL

---
+++ db_postgreSQL.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+def create_table():
+    conn = psycopg2.connect("dbname = 'testdb' user = 'ice' password = '123456' host = '127.0.0.1' port = '5432'")
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)')
+    conn.commit()
+    conn.close()
+
+def insert(item, quantity, price):
+    conn = psycopg2.connect("dbname = 'testdb' user = 'ice' password = '123456' host = '127.0.0.1' port = '5432'")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (%s, %s, %s)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = psycopg2.connect("dbname = 'testdb' user = 'ice' password = '123456' host = '127.0.0.1' port = '5432'")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM store')
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(item):
+    conn = psycopg2.connect("dbname = 'testdb' user = 'ice' password = '123456' host = '127.0.0.1' port = '5432'")
+    cur = conn.cursor()
+    cur.execute('DELETE FROM store WHERE item = %s', (item, ))
+    conn.commit()
+    conn.close()
+
+def update(item, quantity, price):
+    conn = psycopg2.connect("dbname = 'testdb' user = 'ice' password = '123456' host = '127.0.0.1' port = '5432'")
+    cur = conn.cursor()
+    cur.execute('UPDATE store SET quantity = %s, price = %s WHERE item = %s', (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+
+create_table()
+#insert('ice', 100, 50)
+#delete('ice')
+#update('ice', 110, 60)
+print(view())
